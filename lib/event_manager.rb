796e26dab667501870d89a43d27a396959af49be
lohikaarme <stewart.watt@gmail.com>
2023-04-11 22:21:40 -0700

    Completed iteration 2

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,9 @@
 require 'csv'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
 puts 'Event Manager Initialized!'
 
 contents = CSV.open(
@@ -7,8 +12,9 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
+  zipcode = clean_zipcode(row[:zipcode])
   puts "#{name} #{zipcode}"
 end
